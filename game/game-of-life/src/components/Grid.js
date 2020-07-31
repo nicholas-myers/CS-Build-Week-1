19a980dbed3c20b1ceac7d5a744e9c6b049a18ca
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-07-31 03:00:54 -0700

    added comments in code to explain what each section is doing

--- game/game-of-life/src/components/Grid.js
+++ game/game-of-life/src/components/Grid.js
@@ -39,50 +39,69 @@ const connections = [
 export default function Grid() {
   // set the size of the grid
   const [gridSize, setGridSize] = useState(25);
+  // set the initial speed of the game
   const [speed, setSpeed] = useState(150)
-  // create a number of divs per the amount of columns and rows represented by empty x ann y arrays
-  // let x = [];
+  
   // create an empty grid
-
-
   const emptyGrid = () => {
+    // we create an array
     const x = [];
+    // push a new array for each grid size
     for (let i = 0; i < gridSize; i++) {
+      // this creates a 2d array
       x.push(Array.from(Array(gridSize), () => 0));
     }
+    // this 2d array is return to be used in the grid state
     return x;
   };
+  // we set the initial state of the array to run the function empty grid which sets grid to be the 2d array
   const [grid, setGrid] = useState(() => {
     return emptyGrid();
   });
+  // re-render the grid to proper amount of cells upon grid size change
   useEffect(()=> {
     setGrid(emptyGrid())
   }, [gridSize])
 
+  // set a state of whether our game is running or not
   const [running, setRunning] = useState(false);
+  // create a reference to this state to be used in the callback to run the game
   const runningRef = useRef(running);
   runningRef.current = running;
+
+
   const runGame = useCallback(() => {
+    // if running is false stop running the game
     if (!runningRef.current) {
       return;
     }
-    // simulate
+    // run the game
     setGrid((g) => {
-      console.log(g)
+      // immer produce creates a copy of the original grid, a double buffer
+      // this ensures that we don't mutate the original grid and can get the next stage in the life cycle
       return produce(g, (gridCopy) => {
+        // if we think of  our array in terms of x and y axis we check the original array for certain conditions
         for (let x = 0; x < gridSize; x++) {
           for (let y = 0; y < gridSize; y++) {
+            // count the number of live neighbors
             let neighbors = 0;
+            // iterate through the possible connections to find all the neighbors
             connections.forEach(([r, c]) => {
+              // create new x and y coordinates for our 2d array
               const newX = x + r;
               const newY = y + c;
+              // check if its a coordinate on the grid
+              // count the number of live neighbors
               if (newX >= 0 && newX < gridSize && newY >= 0 && newY < gridSize) {
                 neighbors += g[newX][newY];
               }
             });
-
+            // the cell dies from under or overpopulation
+            // combining the two rules together
+            // this also combines the rules on whether the cell will stay alive
             if (neighbors < 2 || neighbors > 3) {
               gridCopy[x][y] = 0;
+              // will the cell be birthed from the right condition of exactly 3 live neighbors
             } else if (g[x][y] === 0 && neighbors === 3) {
               gridCopy[x][y] = 1;
             }
@@ -90,8 +109,9 @@ export default function Grid() {
         }
       });
     });
-
+    // set the speed of the lifecycle
     setTimeout(runGame, speed);
+    // speed and gridsize are in the dependency array so that when they change our game will update properly
   }, [speed, gridSize]);
 
   // console.log(grid);
